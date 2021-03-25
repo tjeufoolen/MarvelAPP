5a4fd1b2f0042f66a4ed928a014b4f7cd010eaf4
killermi200 <killermi200@gmail.com>
2021-03-25 17:51:06 +0100

    added paginated response method to repository

--- app/src/main/java/nl/avans/marvelapp/services/Repository.kt
+++ app/src/main/java/nl/avans/marvelapp/services/Repository.kt
@@ -5,9 +5,7 @@ import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.beust.klaxon.Klaxon
 import nl.avans.marvelapp.R
-import nl.avans.marvelapp.models.Character
 import nl.avans.marvelapp.services.utils.VolleyRequestQueue
 import org.json.JSONObject
 import java.math.BigInteger
@@ -22,6 +20,13 @@ abstract class Repository<T> constructor(private val context: Context, private v
         }
     }
 
+    fun getPaginated(pageOffset: Int, callback: (List<T>?) -> Unit) {
+        executeRequest("", mapOf("offset" to pageOffset.toString())) { response ->
+            callback(convertArray(response))
+        }
+
+    }
+
     fun getById(id: Int, callback: (T?) -> Unit) {
         executeRequest("/$id") { response ->
             callback(convert(response))
@@ -32,9 +37,9 @@ abstract class Repository<T> constructor(private val context: Context, private v
     protected abstract fun convertArray(json: JSONObject): List<T>?
 
     // region Helpers
-    private fun executeRequest(endpoint: String = "", callback: Response.Listener<JSONObject>) {
+    private fun executeRequest(endpoint: String = "", queryParams: Map<String, String>? = null, callback: Response.Listener<JSONObject>) {
         VolleyRequestQueue.getInstance(context).addToRequestQueue(
-            JsonObjectRequest(Request.Method.GET, createRequestUrl(endpoint), null, callback,
+            JsonObjectRequest(Request.Method.GET, createRequestUrl(endpoint, queryParams), null, callback,
                 {
                     Log.d(Log.ERROR.toString(), it.message.toString())
                 }
@@ -42,25 +47,58 @@ abstract class Repository<T> constructor(private val context: Context, private v
         )
     }
 
-    private fun createRequestUrl(endpoint: String) : String {
+    private fun createRequestUrl(endpoint: String, queryParams: Map<String, String>? = null) : String {
         // Build url
         var requestUrl = "$url/$genericEndPoint$endpoint"
 
-        // Append authentication
-        val publicKey = context.resources.getString(R.string.api_public_key)
-        val privateKey = context.resources.getString(R.string.api_private_key)
-        val timestamp = 1
-        val hash = stringToMD5("$timestamp$privateKey$publicKey")
+        // Create map of all query params
+        val allQueryParams: MutableMap<String, String> = mutableMapOf()
+        if (queryParams != null) {
+            allQueryParams.putAll(queryParams)
+        }
 
-        requestUrl += "?apikey=$publicKey&ts=$timestamp&hash=$hash"
+        // Append authentication to query params
+        allQueryParams.putAll(generateAuthenticationHeaders())
+
+        // Append query string to the request url
+        requestUrl += appendQueryString(allQueryParams)
 
         // Return full url
+        Log.d("DEBUGSTRING", requestUrl)
+
         return requestUrl
     }
 
     private fun stringToMD5(input: String): String {
         val md = MessageDigest.getInstance("MD5")
         return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+    }
+
+    private fun appendQueryString(input: Map<String, String>) : String {
+        if (input.isEmpty()) return ""
+
+        var returnString = "?"
+        val stringBuilder = StringBuilder()
+        for((key, value) in input){
+            if(stringBuilder.isNotEmpty()){
+                stringBuilder.append("&")
+            }
+            stringBuilder.append("$key=$value")
+        }
+        returnString+=stringBuilder.toString()
+
+        return returnString
+    }
+
+    private fun generateAuthenticationHeaders(): Map<String, String>{
+        val publicKey = context.resources.getString(R.string.api_public_key)
+        val privateKey = context.resources.getString(R.string.api_private_key)
+        val timestamp = 1
+        val hash = stringToMD5("$timestamp$privateKey$publicKey")
+
+        return mapOf(
+            "apikey" to "$publicKey&ts=$timestamp&hash=$hash"
+        )
     }
     // endregion Helpers
 }
