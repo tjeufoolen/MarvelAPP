6dbd7326d39468f0060bb3ec31e07b72b87a12c0
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-03-28 17:19:04 +0200

    Moved list view code and added current state color to navbar

--- app/src/main/java/nl/avans/marvelapp/fragments/CharactersFragment.kt
+++ app/src/main/java/nl/avans/marvelapp/fragments/CharactersFragment.kt
@@ -1,58 +1,95 @@
 package nl.avans.marvelapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import nl.avans.marvelapp.R
+import nl.avans.marvelapp.RecyclerViewAdapter
+import nl.avans.marvelapp.models.Character
+import nl.avans.marvelapp.services.CharacterRepository
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CharactersFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CharactersFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var characterRepository: CharacterRepository
+    private var recyclerView: RecyclerView? = null
+    private var recyclerViewAdapter: RecyclerViewAdapter? = null
+    var rowsArrayList: ArrayList<Character?> = ArrayList()
+    var isLoading = false
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_characters, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CharactersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                CharactersFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        characterRepository = CharacterRepository(view.context)
+
+        populateData()
+        initAdapter()
+        initScrollListener()
+    }
+
+    private fun populateData() {
+        characterRepository.getAll { characterList ->
+            characterList?.forEach{ character ->
+                rowsArrayList.add(character)
+            }
+            recyclerViewAdapter!!.notifyDataSetChanged()
+        }
+    }
+
+    private fun initAdapter() {
+        recyclerViewAdapter = RecyclerViewAdapter(rowsArrayList)
+        recyclerView!!.adapter = recyclerViewAdapter
+    }
+
+    private fun initScrollListener() {
+        recyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val linearLayoutManager = recyclerView.layoutManager as LinearLayoutManager?
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == rowsArrayList.size - 1) {
+                        //bottom of list!
+                        recyclerView.post {
+                            loadMore()
+                        }
+                        isLoading = true
                     }
                 }
+            }
+        })
     }
+
+    private fun loadMore() {
+        rowsArrayList.add(null)
+        recyclerViewAdapter!!.notifyItemInserted(rowsArrayList.size - 1)
+        Handler(Looper.getMainLooper()).postDelayed({
+            rowsArrayList.removeAt(rowsArrayList.size - 1)
+            val scrollPosition: Int = rowsArrayList.size
+            recyclerViewAdapter!!.notifyItemRemoved(scrollPosition)
+
+            characterRepository.getPaginated(scrollPosition) { newCharacters ->
+                newCharacters?.forEach { character ->
+                    rowsArrayList.add(character)
+                }
+                recyclerViewAdapter!!.notifyDataSetChanged()
+                isLoading = false
+            }
+
+        }, 2000)
+    }
+
 }
