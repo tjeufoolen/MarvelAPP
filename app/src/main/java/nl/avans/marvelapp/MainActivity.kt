aad6df2a3192fff6498168a63250e42b52293f15
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-03-28 15:53:42 +0200

    Feature/list of characters (#2)
    * inbetween
    
    * Update .gitignore
    
    * inbetween
    
    * Fixed error message when executerequest failed
    
    * JSON model binding
    
    * added paginated response method to repository
    
    * initial infinite scroll attempt
    
    added infinite scroll capabilities using https://www.journaldev.com/24041/android-recyclerview-load-more-endless-scrolling
    
    * Update MainActivity.kt
    
    * cleanup
    
    Co-authored-by: killermi200 <killermi200@gmail.com>

--- app/src/main/java/nl/avans/marvelapp/MainActivity.kt
+++ app/src/main/java/nl/avans/marvelapp/MainActivity.kt
@@ -1,25 +1,80 @@
 package nl.avans.marvelapp
 
 import android.os.Bundle
-import android.widget.TextView
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import nl.avans.marvelapp.models.Character
 import nl.avans.marvelapp.services.CharacterRepository
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var characterRepository: CharacterRepository
+    private var recyclerView: RecyclerView? = null
+    private var recyclerViewAdapter: RecyclerViewAdapter? = null
+    var rowsArrayList: ArrayList<Character?> = ArrayList()
+    var isLoading = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recyclerView)
+        characterRepository = CharacterRepository(this)
 
-//        CharacterRepository(this).getAll { array ->
-//            if (array != null) {
-//                findViewById<TextView>(R.id.message).text = array[0].thumbnail.url
-//            }
-//        }
+        populateData()
+        initAdapter()
+        initScrollListener()
+    }
 
-        CharacterRepository(this).getById(1011334) { obj ->
-            if (obj != null) {
-                findViewById<TextView>(R.id.message).text = obj.thumbnail.url
+    private fun populateData() {
+        characterRepository.getAll { characterList ->
+            characterList?.forEach{ character ->
+                rowsArrayList.add(character)
             }
+            recyclerViewAdapter!!.notifyDataSetChanged()
         }
+    }
+
+    private fun initAdapter() {
+        recyclerViewAdapter = RecyclerViewAdapter(rowsArrayList)
+        recyclerView!!.adapter = recyclerViewAdapter
+    }
+
+    private fun initScrollListener() {
+        recyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val linearLayoutManager = recyclerView.layoutManager as LinearLayoutManager?
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == rowsArrayList.size - 1) {
+                        //bottom of list!
+                        recyclerView.post {
+                            loadMore()
+                        }
+                        isLoading = true
+                    }
+                }
+            }
+        })
+    }
+
+    private fun loadMore() {
+        rowsArrayList.add(null)
+        recyclerViewAdapter!!.notifyItemInserted(rowsArrayList.size - 1)
+        Handler(Looper.getMainLooper()).postDelayed({
+            rowsArrayList.removeAt(rowsArrayList.size - 1)
+            val scrollPosition: Int = rowsArrayList.size
+            recyclerViewAdapter!!.notifyItemRemoved(scrollPosition)
+
+            characterRepository.getPaginated(scrollPosition) { newCharacters ->
+                newCharacters?.forEach { character ->
+                    rowsArrayList.add(character)
+                }
+                recyclerViewAdapter!!.notifyDataSetChanged()
+                isLoading = false
+            }
+
+        }, 2000)
     }
 }
--- app/src/main/java/nl/avans/marvelapp/services/Repository.kt
+++ app/src/main/java/nl/avans/marvelapp/services/Repository.kt
@@ -5,9 +5,7 @@ import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.beust.klaxon.Klaxon
 import nl.avans.marvelapp.R
-import nl.avans.marvelapp.models.Character
 import nl.avans.marvelapp.services.utils.VolleyRequestQueue
 import org.json.JSONObject
 import java.math.BigInteger
@@ -22,6 +20,13 @@ abstract class Repository<T> constructor(private val context: Context, private v
         }
     }
 
+    fun getPaginated(itemOffset: Int, callback: (List<T>?) -> Unit) {
+        executeRequest("", mapOf("offset" to itemOffset.toString())) { response ->
+            callback(convertArray(response))
+        }
+
+    }
+
     fun getById(id: Int, callback: (T?) -> Unit) {
         executeRequest("/$id") { response ->
             callback(convert(response))
@@ -32,35 +37,66 @@ abstract class Repository<T> constructor(private val context: Context, private v
     protected abstract fun convertArray(json: JSONObject): List<T>?
 
     // region Helpers
-    private fun executeRequest(endpoint: String = "", callback: Response.Listener<JSONObject>) {
+    private fun executeRequest(endpoint: String = "", queryParams: Map<String, String>? = null, callback: Response.Listener<JSONObject>) {
         VolleyRequestQueue.getInstance(context).addToRequestQueue(
-            JsonObjectRequest(Request.Method.GET, createRequestUrl(endpoint), null, callback,
+            JsonObjectRequest(Request.Method.GET, createRequestUrl(endpoint, queryParams), null, callback,
                 {
-                    Log.d(Log.ERROR.toString(), it.message.toString())
+                    // Frontend should ignore the fact that a request can go wrong and handle this
+                    // already in its own way. However, we keep logging the error so that we as
+                    // developers, have an easier time debugging a problem.
+                    Log.d("Error", it.message.toString())
                 }
             )
         )
     }
 
-    private fun createRequestUrl(endpoint: String) : String {
+    private fun createRequestUrl(endpoint: String, queryParams: Map<String, String>? = null): String {
         // Build url
         var requestUrl = "$url/$genericEndPoint$endpoint"
 
-        // Append authentication
-        val publicKey = context.resources.getString(R.string.api_public_key)
-        val privateKey = context.resources.getString(R.string.api_private_key)
-        val timestamp = 1
-        val hash = stringToMD5("$timestamp$privateKey$publicKey")
+        // Create map of all query params
+        val allQueryParams: MutableMap<String, String> = mutableMapOf()
+        if (queryParams != null) {
+            allQueryParams.putAll(queryParams)
+        }
 
-        requestUrl += "?apikey=$publicKey&ts=$timestamp&hash=$hash"
+        // Append authentication to query params
+        allQueryParams.putAll(generateAuthenticationHeaders())
 
-        // Return full url
+        // Append query string to the request url
+        requestUrl += appendQueryString(allQueryParams)
+
         return requestUrl
     }
 
     private fun stringToMD5(input: String): String {
         val md = MessageDigest.getInstance("MD5")
         return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+    }
+
+    private fun appendQueryString(input: Map<String, String>): String {
+        if (input.isEmpty()) return ""
+
+        var returnString = "?"
+        val stringBuilder = StringBuilder()
+        for ((key, value) in input) {
+            if (stringBuilder.isNotEmpty()) {
+                stringBuilder.append("&")
+            }
+            stringBuilder.append("$key=$value")
+        }
+        returnString += stringBuilder.toString()
+
+        return returnString
+    }
+
+    private fun generateAuthenticationHeaders(): Map<String, String> {
+        val publicKey = context.resources.getString(R.string.api_public_key)
+        val privateKey = context.resources.getString(R.string.api_private_key)
+        val timestamp = 1
+        val hash = stringToMD5("$timestamp$privateKey$publicKey")
+
+        return mapOf("apikey" to "$publicKey&ts=$timestamp&hash=$hash")
     }
     // endregion Helpers
 }
