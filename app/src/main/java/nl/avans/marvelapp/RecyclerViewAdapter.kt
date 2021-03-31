cc1860d1312fa9aa5010a6b96a16c8d90dcdbdcb
Micha Nijenhof <killermi200@gmail.com>
2021-03-31 11:57:32 +0200

    Feature/share character (#13)
    * added intent for sharing character
    
    * added share-comic
    
    * Update RecyclerViewAdapter.kt

--- app/src/main/java/nl/avans/marvelapp/RecyclerViewAdapter.kt
+++ app/src/main/java/nl/avans/marvelapp/RecyclerViewAdapter.kt
@@ -44,11 +44,11 @@ abstract class RecyclerViewAdapter(private var itemList: List<IRowObject?>?) : R
         var tvItem: TextView = itemView.findViewById(R.id.tvItem)
     }
 
-    protected inner class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var progressBar: ProgressBar = itemView.findViewById(R.id.progressBar)
-    }
+    protected inner class LoadingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     protected fun showLoadingView(viewHolder: LoadingViewHolder, position: Int) {
+        val pos = position + viewHolder.adapterPosition
+        pos + 1
         //ProgressBar would be displayed
     }
 
--- app/src/main/java/nl/avans/marvelapp/fragments/comics/ComicDetailFragment.kt
+++ app/src/main/java/nl/avans/marvelapp/fragments/comics/ComicDetailFragment.kt
@@ -1,16 +1,19 @@
 package nl.avans.marvelapp.fragments.comics
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.android.volley.toolbox.NetworkImageView
 import nl.avans.marvelapp.R
 import nl.avans.marvelapp.models.Comic
 import nl.avans.marvelapp.repositories.utils.VolleyRequestQueue
-import org.w3c.dom.Text
 
 private const val COMIC_ARGUMENT = "comic"
 
@@ -44,6 +47,14 @@ class ComicDetailFragment : Fragment() {
         view.findViewById<TextView>(R.id.tvComicDetailPagesValue)?.text = comic?.pageCount?.toString()
         setDescription(comic?.description)
         setImage(view)
+        setButton(view)
+    }
+
+    private fun setButton(view: View) {
+        val btn = view.findViewById<Button>(R.id.btnComicDetailShare)
+        btn.setOnClickListener {
+            share()
+        }
     }
 
     private fun setImage(view: View) {
@@ -54,6 +65,7 @@ class ComicDetailFragment : Fragment() {
         photoView.setErrorImageResId(R.drawable.error)
         photoView.setImageUrl(imgUrl, imgLoader)
     }
+
     private fun setDescription(description: String?){
         val textView = view?.findViewById<TextView>(R.id.tvComicDetailDescription)
 
@@ -63,6 +75,41 @@ class ComicDetailFragment : Fragment() {
         }
 
         textView?.text = context?.resources?.getString(R.string.description_unavailable)
+    }
+
+    private fun share(){
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, generateShareDescription())
+        try {
+            requireActivity().startActivity(intent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(context, context?.resources?.getString(R.string.no_shareable_app_installed), Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun generateShareDescription() :String{
+        var string = ""
+
+        val shareGreeting = context?.resources?.getString(R.string.share_comic_greeting)
+        val shareMentionDescription = context?.resources?.getString(R.string.share_comic_mention_description)
+        val shareLookAtHim = context?.resources?.getString(R.string.share_comic_look_at_it)
+
+        string += "$shareGreeting\n"
+        string += comic?.title
+        string += "\n\n"
+
+        if(!comic?.description.isNullOrEmpty()){
+            string += "$shareMentionDescription\n"
+            string += comic?.description
+            string += "\n\n"
+        }
+
+        string += "$shareLookAtHim\n"
+        string += comic?.thumbnail?.url
+
+        return string
     }
 
     companion object {
